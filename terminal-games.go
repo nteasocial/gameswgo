d10a78a31680a70b545c6abfdf385e631d44b630
nwunderly <48489521+nwunderly@users.noreply.github.com>
2020-12-30 22:20:48 -0500

    update main module

--- terminal-games.go
+++ terminal-games.go
@@ -2,34 +2,20 @@ package main
 
 import (
 	"fmt"
-	"github.com/akamensky/argparse"
 	"github.com/nwunderly/terminal-games/blackjack"
 	"github.com/nwunderly/terminal-games/tic-tac-toe"
 	"os"
 )
 
 func main() {
-	parser := argparse.NewParser("terminal-games", "")
-	game := parser.String("g", "game", &argparse.Options{
-		Required: false,
-		Validate: nil,
-		Help:     "",
-		Default:  nil,
-	})
-
-	err := parser.Parse(os.Args)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	switch *game {
+	game := os.Args[1]
+	switch game {
 	case "blackjack":
 		blackjack.New().Run()
 	case "tic-tac-toe":
 	case "ttt":
 		tic_tac_toe.New().Run()
 	default:
+		panic(fmt.Errorf("INVALID GAME NAME: %s", game))
 	}
-
 }
